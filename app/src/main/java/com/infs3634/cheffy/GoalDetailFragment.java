03c89e6502d2b70d1ec2b5b6fbe464d589149d1e
EmmaStyles <e.styles@student.unsw.edu.au>
2020-04-10 11:10:08 +1000

    Layout

---
+++ app/src/main/java/com/infs3634/cheffy/GoalDetailFragment.java
@@ -0,0 +1,72 @@
+package com.infs3634.cheffy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.infs3634.cheffy.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Goal detail screen.
+ * This fragment is either contained in a {@link GoalListActivity}
+ * in two-pane mode (on tablets) or a {@link GoalDetailActivity}
+ * on handsets.
+ */
+public class GoalDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public GoalDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(mItem.content);
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.goal_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.goal_detail)).setText(mItem.details);
+        }
+
+        return rootView;
+    }
+}
