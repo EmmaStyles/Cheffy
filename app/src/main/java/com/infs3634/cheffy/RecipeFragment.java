b028fbade16b8b4d27d942024d2b9a3a240c95b3
serenathai <z5159999@unsw.edu.au>
2020-04-24 13:44:34 +1000

    Added images to the recipe fragment

--- app/src/main/java/com/infs3634/cheffy/RecipeFragment.java
+++ app/src/main/java/com/infs3634/cheffy/RecipeFragment.java
@@ -8,7 +8,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 
 public class RecipeFragment extends Fragment {
@@ -55,6 +58,10 @@ public class RecipeFragment extends Fragment {
     private void updateData(){
         View rootView = getView();
         if (rootView!=null && mMeal != null){
+            Glide.with(rootView)
+                    .load(mMeal.getStrMealThumb() + "/preview")
+                    .override(200,200)
+                    .into((ImageView)rootView.findViewById(R.id.recipeImageView));
             ((TextView) rootView.findViewById(R.id.recipeName)).setText(mMeal.getStrMeal());
             ((TextView) rootView.findViewById(R.id.ingredient1)).setText(mMeal.getStrIngredient1());
             ((TextView) rootView.findViewById(R.id.ingredient2)).setText(mMeal.getStrIngredient2());
