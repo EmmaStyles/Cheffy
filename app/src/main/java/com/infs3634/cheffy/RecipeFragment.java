864e36cd6f29e922af329de810c61d69939afb10
serenathai <z5159999@unsw.edu.au>
2020-04-24 20:10:20 +1000

    Added something to the manifest file that fixes a crash that would occur if you clicked on a particular recipe

--- app/src/main/java/com/infs3634/cheffy/RecipeFragment.java
+++ app/src/main/java/com/infs3634/cheffy/RecipeFragment.java
@@ -85,6 +85,5 @@ public class RecipeFragment extends Fragment {
             ((TextView) rootView.findViewById(R.id.measure10)).setText(mMeal.getStrMeasure10());
             ((TextView) rootView.findViewById(R.id.instructionsTextView)).setText(mMeal.getStrInstructions());
         }
-
     }
 }
