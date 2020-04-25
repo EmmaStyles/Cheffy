437d1f1379c1cd556ebb3ec7f15f69d8badeba92
EmmaStyles <e.styles@student.unsw.edu.au>
2020-04-25 16:48:35 +1000

    Achievements activity added plus main activity UI

---
+++ app/src/main/java/com/infs3634/cheffy/AchievementsActivity.java
@@ -0,0 +1,16 @@
+package com.infs3634.cheffy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+
+import android.os.Bundle;
+
+public class AchievementsActivity extends AppCompatActivity {
+    private String TAG = "AchievementsActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achievements);
+    }
+}
--- app/src/main/java/com/infs3634/cheffy/MainActivity.java
+++ app/src/main/java/com/infs3634/cheffy/MainActivity.java
@@ -12,6 +12,7 @@ import com.timqi.sectorprogressview.ColorfulRingProgressView;
 public class MainActivity extends AppCompatActivity {
     private Button mButton;
     private Button recipeButton;
+    private Button achievementsButton;
     private ColorfulRingProgressView crpv;
     public static final String EXTRA_MESSAGE = "com.infs3634.cheffy.MESSAGE";
     private String TAG = "MainActivity";
@@ -22,10 +23,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mButton = findViewById(R.id.btnGoals);
         recipeButton = findViewById(R.id.recipesButton);
+        achievementsButton = findViewById(R.id.btnAchievements);
         crpv = findViewById(R.id.crpv);
         // finish percent of progress
         int count = Goal.countCheckedGoals();
-        crpv.setPercent(count);
+        crpv.setPercent(75);
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +43,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        achievementsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                launchAchievementsActivity("AchievementsActivity");
+            }
+        });
 
     }
 
@@ -52,6 +60,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void launchRecipesActivity(String message){
         Intent intent = new Intent(this, MealsMainActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    private void launchAchievementsActivity(String message){
+        Intent intent = new Intent(this, AchievementsActivity.class);
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
