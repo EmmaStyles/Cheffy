590e86cd127e65c01f94b26cc6f2c220c59f9c34
serenathai <z5159999@unsw.edu.au>
2020-04-26 11:56:33 +1000

    Made achievement badges circular

--- app/src/main/java/com/infs3634/cheffy/AchievementsActivity.java
+++ app/src/main/java/com/infs3634/cheffy/AchievementsActivity.java
@@ -6,15 +6,21 @@ import android.util.Log;
 import android.os.Bundle;
 import android.widget.TextView;
 
-//Establishes AcievementsActivity
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+//Establishes AchievementsActivity
 public class AchievementsActivity extends AppCompatActivity {
     private String TAG = "AchievementsActivity";
     public TextView mLevel;
+    public CircularImageView circleStar;
+    public CircularImageView circleWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievements);
         mLevel = findViewById(R.id.tvLevel);
+        circleStar = findViewById(R.id.circleStar);
+        circleWorld = findViewById(R.id.circleWorld);
     }
 }
